be222116860be034afae2457f3bf59f7ed8c15b9
Rene Hexel <rhexel+gitcommit@me.com>
2012-01-08 10:17:36 +1000

    Fix typo: make suspend states actually work

--- gufsm/FSMWBSubMachineFactory.cc
+++ gufsm/FSMWBSubMachineFactory.cc
@@ -85,7 +85,7 @@ bool WBSubMachineFactory::determineSuspendState(const char *name)
 {
         if (machine()->suspendState()) return true;
 
-        for (StateVector::const_iterator i = machine()->states().begin(); i != machine()->states().begin(); i++)
+        for (StateVector::const_iterator i = machine()->states().begin(); i != machine()->states().end(); i++)
         {
                 if ((*i)->transitions().size()) // nonempty, cannot be suspend
                         continue;
@@ -93,7 +93,7 @@ bool WBSubMachineFactory::determineSuspendState(const char *name)
                         continue;               // non-matching name
                 bool transitionsFound = false;
                 StateVector::const_iterator j = machine()->states().begin();
-                for (; j != machine()->states().begin(); j++)
+                for (; j != machine()->states().end(); j++)
                 {
                         if (j == i) continue;   // ignore the tested state
                         for (TransitionVector::const_iterator k = (*j)->transitions().begin(); k != (*j)->transitions().end(); k++)
@@ -109,7 +109,7 @@ bool WBSubMachineFactory::determineSuspendState(const char *name)
                 if (!transitionsFound)          // found my suspend state
                 {
                         machine()->setSuspendState(*i);
-                DBG(cout << "Found syspend state " << (*i)->name() << endl);
+                        DBG(cout << "Found suspend state " << (*i)->name() << endl);
                         return true;
                 }
         }
