ee28a85123f1754bd4251a001e45a2edd2260872
Rene Hexel <rhexel+gitcommit@me.com>
2012-05-31 16:19:59 +1000

    Factor out a vector machine runner (e.g. for use in qfsm)

--- gufsm/FSMVectorRunner.cc
+++ gufsm/FSMVectorRunner.cc
@@ -1,60 +1,10 @@
-/*
- *  FSMachineVector.cc
- *  
- *  Created by René Hexel on 22/11/11.
- *  Copyright (c) 2011 Rene Hexel.
- *  All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above
- *    copyright notice, this list of conditions and the following
- *    disclaimer in the documentation and/or other materials
- *    provided with the distribution.
- *
- * 3. All advertising materials mentioning features or use of this
- *    software must display the following acknowledgement:
- *
- *        This product includes software developed by Rene Hexel.
- *
- * 4. Neither the name of the author nor the names of contributors
- *    may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
- * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * -----------------------------------------------------------------------
- * This program is free software; you can redistribute it and/or
- * modify it under the above terms or under the terms of the GNU
- * General Public License as published by the Free Software Foundation;
- * either version 2 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see http://www.gnu.org/licenses/
- * or write to the Free Software Foundation, Inc., 51 Franklin Street,
- * Fifth Floor, Boston, MA  02110-1301, USA.
- *
- */
+//
+//  FSMVectorRunner.cc
+//  
+//
+//  Created by René Hexel on 31/05/12.
+//  Copyright (c) 2012 Rene Hexel. All rights reserved.
+//
 #include "FSMANTLRMaths.h"
 #include "FSMANTLRContext.h"
 
@@ -72,16 +22,12 @@
 #include "FSMVectorRunner.h"
 
 #define ANTLRFunc(x,n)  x func ## x; \
-                        antlr_context.set_function((n), &func ## x);
+antlr_context.set_function((n), &func ## x);
 #define ANTLRMaths(x)   ANTLRMaths ## x func ## x; \
-                        antlr_context.set_function(func ## x.name(), &func ## x);
+antlr_context.set_function(func ## x.name(), &func ## x);
 
 using namespace std;
 using namespace FSM;
-
-int dump_kripke(StateMachineVectorFactory &factory, vector<string> &machine_names);
-int block_schedule(StateMachineVectorFactory &factory, vector<string> &machine_names);
-int factory_execute(StateMachineVectorFactory &factory, vector<string> &machine_names);
 
 static cdlbridge *gucdlbridge;
 
@@ -176,7 +122,7 @@ class WBSuspendFunction: public PostBoolFunction
 {
 public:
         WBSuspendFunction(std::string name = "suspend"): PostBoolFunction(name, false) {}
-
+        
         virtual void add_parameter(int index, long long value)
         {
                 _type = string("suspend_") + (const char *) value;
@@ -189,7 +135,7 @@ class WBResumeFunction: public WBSuspendFunction
 {
 public:
         WBResumeFunction(): WBSuspendFunction("resume") {}
-
+        
         virtual void add_parameter(int index, long long value)
         {
                 _type = string("suspend_") + (const char *) value;
@@ -202,7 +148,7 @@ class WBRestartFunction: public PostStringFunction
 {
 public:
         WBRestartFunction(std::string name = "restart"): PostStringFunction(name, "") {}
-
+        
         virtual void add_parameter(int index, long long value)
         {
                 PostStringFunction::add_parameter(1, value);
@@ -235,17 +181,17 @@ class WBSayStateFunction: public PostStringFunction
 {
 public:
         WBSayStateFunction(std::string name = "Say"): PostStringFunction(name, "") {}
-
+        
         virtual void performv(Machine *m, ActionStage s, int i, va_list l)
         {
                 stringstream ss;
                 ss << _content << m->currentState()->name();
                 setContent(ss.str().c_str());
-
+                
                 WBPostAction::performv(m, s, i, l);
                 setContent("");
         }
-
+        
         virtual void add_parameter(int index, long long value)
         {
                 PostStringFunction::add_parameter(1, value);
@@ -290,82 +236,66 @@ static void usage(const char *cmd)
 }
 
 
-int dump_kripke(StateMachineVectorFactory &factory, vector<string> &machine_names)
+int run_machine_vector(vector<string> &machine_names, machine_runner_f execute, bool verbose)
 {
-        string kripke = factory.fsms()->kripkeInSVMformat();
-        cout << kripke << endl;
-        return EXIT_SUCCESS;
-}
-
-
-int block_schedule(StateMachineVectorFactory &factory, vector<string> &machine_names)
-{
-        factory.fsms()->scheduleExecuteOnQueue();
+        ANTLRContext antlr_context;             // create whiteboard
         
-        dispatch_main();                        // never returns
-
-        return EXIT_FAILURE;
-}
-
-
-int factory_execute(StateMachineVectorFactory &factory, vector<string> &machine_names)
-{
-        factory.execute();                      // execute synchronously
-        
-        return EXIT_SUCCESS;
-}
-
-int main (int argc, char * const argv[])
-{
-        bool kripke_flag = false, verbose = false, want_cdl = true,
-#ifdef __APPLE__
-        blocks_flag = true;
-#else
-        blocks_flag = false;
+        ANTLRFunc(TimeoutPredicate,     "timeout");
+        ANTLRFunc(SystemFunction,       "system");
+#ifdef NEED_SLEEP
+        ANTLRFunc(SleepFunction,        "sleep");
 #endif
-        int ch;
-        while ((ch = getopt(argc, argv, "BbCkv")) != -1)
+        ANTLRFunc(PrintStatenameFunction, "print_state_name");
+        ANTLRFunc(PrintStringAction,    "print");
+        ANTLRFunc(PrintIntAction,       "print_int");
+        ANTLRFunc(PrintFixedAction,     "print_fixed");
+        ANTLRFunc(PostStringFunction,   "post");
+        ANTLRFunc(PostIntFunction,      "post_int");
+        ANTLRFunc(WBPostIntVecAction,   "postv");
+        ANTLRFunc(WBSuspendFunction,    "suspend");
+        ANTLRFunc(WBResumeFunction,     "resume");
+        ANTLRFunc(WBRestartFunction,    "restart");
+        
+        ANTLRFunc(WBSayFunction,        "say");                 // sends Say
+        ANTLRFunc(WBSpeechFunction,     "speech");              // sends Speech
+        ANTLRFunc(WBSayStateFunction,   "say_state_name");      // sends Say
+        ANTLRFunc(WBSpeakStateFunction, "speak_state_name");    // sends Speech
+        
+        ANTLRFunc(LoadTheoryFunction,   "load_theory");
+        ANTLRFunc(ProofFunction,        "prove");
+        
+        /*
+         * maths functions
+         */
+        ANTLRMaths(Abs);
+        ANTLRMaths(Sign);
+        ANTLRMaths(Random);
+        ANTLRMaths(SRandom);
+        ANTLRMaths(Sin);
+        ANTLRMaths(Cos);
+        ANTLRMaths(Tan);
+        ANTLRMaths(Cot);
+        ANTLRMaths(ATan);
+        ANTLRMaths(ASin);
+        ANTLRMaths(ACos);
+        ANTLRMaths(Log);
+        ANTLRMaths(Ld);
+        ANTLRMaths(Lg);
+        
+        ANTLRMaths(Min);
+        ANTLRMaths(Max);
+        ANTLRMaths(Avg);
+        ANTLRMaths(FTA);
+        ANTLRMaths(GAvg);
+        
+        StateMachineVectorFactory factory(&antlr_context, machine_names);
+
+        if (verbose)
         {
-                switch (ch)
-                {
-                        case 'B':       // don't use blocks (dispatch queues)
-                                blocks_flag = false;
-                                break;
-                        case 'b':       // use blocks (dispatch queues)
-                                blocks_flag = true;
-                                break;
-                        case 'C':       // disable CDL bridge
-                                want_cdl = false;
-                                break;
-                        case 'k':
-                                kripke_flag = true;
-                                break;
-                        case 'v':
-                                verbose = true;
-                                break;
-                        case '?':
-                        default:
-                                usage(argv[0]);
-                }
-        }
-        argc -= optind;
-        argv += optind;
-
-        vector<string> machine_names;
-
-        while (argc-- > 0)
-                machine_names.push_back(*argv++);
-
-
-        if (kripke_flag)
-        {
-                return run_machine_vector(machine_names, dump_kripke, verbose);
-        }
-        if (blocks_flag)                        // execute on dispatch queue
-        {
-                return run_machine_vector(machine_names, block_schedule, verbose);
+                string descr = factory.fsms()->description();
+                cout << descr << endl;
         }
 
-        return run_machine_vector(machine_names, factory_execute, verbose);
+        return execute(factory, machine_names);
 }
 
---
+++ gufsm/FSMVectorRunner.h
@@ -0,0 +1,20 @@
+//
+//  FSMVectorRunner.h
+//  
+//
+//  Created by René Hexel on 31/05/12.
+//  Copyright (c) 2012 Rene Hexel. All rights reserved.
+//
+
+#ifndef _FSMVectorRunner_h
+#define _FSMVectorRunner_h
+
+#include <vector>
+#include <string>
+#include "FSMVectorFactory.h"
+
+typedef int (*machine_runner_f)(FSM::StateMachineVectorFactory &factory, std::vector<std::string> &machine_names);
+
+int run_machine_vector(std::vector<std::string> &machine_names, machine_runner_f execute, bool verbose = false);
+
+#endif
