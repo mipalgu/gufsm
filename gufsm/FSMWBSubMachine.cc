1323935fc4f553b8d7b9af1addab6a86d0b8a8d2
Robert Coleman <robertc.softmyn@gmail.com>
2012-06-02 13:42:04 +1000

    Post name/machine id pair to the whiteboard if machine is being monitored.

--- gufsm/FSMWBSubMachine.cc
+++ gufsm/FSMWBSubMachine.cc
@@ -207,23 +207,32 @@ bool WBSubMachine::executeOnce(bool *fired)
         else if (_scheduleRestart) restart();
         else if (_scheduleResume) resume();
 	
-	/* Post the name and id of this machine to the whiteboard, so
-	 * that a monitoring module can identify the machines without
-	 * their name. Post a number of times incase the messages don't
-	 * make it to the whiteboard. */
+	
 	if ( isBeingMonitored() ) {
+		/* Post the name and id of this machine to the whiteboard, so
+		 * that a monitoring module can identify the machines without
+		 * their name. Post a number of times incase the messages don't
+		 * make it to the whiteboard. */
 		int mpostcount = getMachineIdPostCount();
-		if ( mpostcount < TIMES_TO_POST_NAME_AND_ID ) {
-			ANTLRContext * c = (ANTLRContext *) context();
-			
+		ANTLRContext * c = (ANTLRContext *) context();
+		if ( mpostcount < TIMES_TO_POST_NAME_AND_ID ) 
+		{
 			char msgName[60];
 			msgName[59] = 0;
 			WBMsg mid(id());
-			sprintf(msgName, "indexof_%s", name().c_str());
+			sprintf(msgName, "gu_indexof_%s", name().c_str());
 			c->whiteboard()->addMessage(msgName, mid);
 			
 			setMachineIdPostCount(++mpostcount);
 		}
+		
+		/* Post the running state id. */
+		char msgName[60];
+		msgName[59] = 0;
+		int csid = currentStateID();
+		WBMsg msg(csid);
+		sprintf(msgName, "mon_c_state_m:%d", id());
+		c->whiteboard()->addMessage(msgName, msg);
 	}
 	
         return SuspensibleMachine::executeOnce(fired);
