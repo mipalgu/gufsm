46b810039b37c6cc3124052f0e8d92cdc1f27ab5
Rene Hexel <r.hexel@griffith.edu.au>
2012-10-12 05:41:14 +1000

    Add compiled-machine front end.

---
+++ clfsm/clfsm_machine.cc
@@ -0,0 +1,9 @@
+//
+//  clfsm_machine.cc
+//  gufsm
+//
+//  Created by Rene Hexel on 11/10/12.
+//  Copyright (c) 2012 ICT. All rights reserved.
+//
+
+#include "clfsm_machine.h"
---
+++ clfsm/clfsm_machine.h
@@ -0,0 +1,14 @@
+//
+//  clfsm_machine.h
+//  gufsm
+//
+//  Created by Rene Hexel on 11/10/12.
+//  Copyright (c) 2012 ICT. All rights reserved.
+//
+
+#ifndef __gufsm__clfsm_machine__
+#define __gufsm__clfsm_machine__
+
+#include <iostream>
+
+#endif /* defined(__gufsm__clfsm_machine__) */
