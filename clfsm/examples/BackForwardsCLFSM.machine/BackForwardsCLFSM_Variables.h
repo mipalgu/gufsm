69f3666d57590ba139b335133b83805b1bd44794
vlad <v.estivill-castro@griffith.edu.au>
2013-01-23 16:04:21 +1000

    translated to clfsm

--- clfsm/examples/BackForwardsCLFSM.machine/BackForwardsCLFSM_Variables.h
+++ clfsm/examples/BackForwardsCLFSM.machine/BackForwardsCLFSM_Variables.h
@@ -1,5 +1,5 @@
 //
-// BackForwardsCLFSM_Variables.h -- 2013-01-23 05:55:50 +0000
+// BackForwardsCLFSM_Variables.h -- 2013-01-23 06:03:42 +0000
 //
 // Automatically created through MiCASE -- do not change manually!
 //
--- clfsm/examples/BackForwardsCLFSM.machine/State_Pong.h
+++ clfsm/examples/BackForwardsCLFSM.machine/State_Pong.h
@@ -1,5 +1,5 @@
 //
-// State_Pong.h -- 2013-01-23 05:55:50 +0000
+// State_Pong.h -- 2013-01-23 06:03:42 +0000
 //
 // Automatically created through MiCASE -- do not change manually!
 //
