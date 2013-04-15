e48c4a60d0e4b490dda9fd73c713cf9c8a87a27d
vlad <v.estivill-castro@griffith.edu.au>
2013-04-15 16:25:58 +1000

    Works now, trivial states

--- clfsm/examples/GameController_FromUDP.machine/GameController_FromUDP_Variables.h
+++ clfsm/examples/GameController_FromUDP.machine/GameController_FromUDP_Variables.h
@@ -1,5 +1,5 @@
 //
-// GameController_FromUDP_Variables.h -- 2013-04-15 05:10:33 +0000
+// GameController_FromUDP_Variables.h -- 2013-04-15 06:23:39 +0000
 //
 // Automatically created through MiCASE -- do not change manually!
 //
