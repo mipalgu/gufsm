d5fd117606257d7fbe8f36a3232bd2535420bb1a
Vlad Estivill-Castro <v.estivill-castro@griffith.edu.au>
2013-04-22 10:01:53 +1000

    Better layout

--- clfsm/examples/GameController_FromUDP.machine/State_Init.h
+++ clfsm/examples/GameController_FromUDP.machine/State_Init.h
@@ -1,5 +1,5 @@
 //
-// State_Init.h -- 2013-04-21 23:42:51 +0000
+// State_Init.h -- 2013-04-21 23:58:10 +0000
 //
 // Automatically created through MiCASE -- do not change manually!
 //
--- clfsm/examples/GameController_FromUDP.machine/State_Init_VarRefs.mm
+++ clfsm/examples/GameController_FromUDP.machine/State_Init_VarRefs.mm
@@ -1,5 +1,5 @@
 //
-// State_Init_VarRefs.mm -- 2013-04-21 23:42:51 +0000
+// State_Init_VarRefs.mm -- 2013-04-21 23:58:10 +0000
 //
 // Automatically created through MiCASE -- do not change manually!
 //
