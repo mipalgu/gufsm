897c548a1b9affa4ff95e462bcd0aadaa4abc87a
Rene Hexel <r.hexel@griffith.edu.au>
2013-05-28 17:50:11 +1000

    add missing files

---
+++ clfsm/machines/RoboCup2013/StateMachineStarter.machine/State_Say_IP_OnEntry.mm
@@ -0,0 +1,6 @@
+stringstream ss;
+PlayerNumber_t myPlayerNumber;
+
+ss << "Player " << int(myPlayerNumber()) << ": ";
+
+say << ss.str();
---
+++ clfsm/machines/RoboCup2013/StateMachineStarter.machine/State_Say_IP_VarRefs.mm
@@ -0,0 +1,13 @@
+//
+// State_Say_IP_VarRefs.mm
+//
+// Automatically created through MiCASE -- do not change manually!
+//
+Say_IP *_s = static_cast<Say_IP *>(_state);
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-variable"
+#pragma clang diagnostic ignored "-Wshadow"
+
+
+#pragma clang diagnostic pop
