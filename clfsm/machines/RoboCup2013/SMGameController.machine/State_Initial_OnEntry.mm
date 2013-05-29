396a9a95f224482240ed3cfe909e008089de4076
Rene Hexel <r.hexel@griffith.edu.au>
2013-05-29 16:17:53 +1000

    Set chest colour to team colour in initial

--- clfsm/machines/RoboCup2013/SMGameController.machine/State_Initial_OnEntry.mm
+++ clfsm/machines/RoboCup2013/SMGameController.machine/State_Initial_OnEntry.mm
@@ -11,6 +11,7 @@ postGS(smGameState);
 gameState = smGameState.theGSGameState();
 
 SENSORS_LedsSensors ledValues = leds.get();
+ledValues.LEDsGroupChange(Chest, ourTeamColour == TeamBlue ? Blue : Red);
 ledValues.LEDsGroupChange(LFoot, ourTeamColour == TeamBlue ? Blue : Red);
 ledValues.LEDsGroupChange(RFoot, kickoffTeam == TeamBlue ? Blue : Red);
 leds.set(ledValues);
