5b0792ad4edd725aeba0dc8cf287764e93d309c0
Robert Coleman <robertc.softmyn@gmail.com>
2013-05-29 11:33:38 +1000

    Created SMKicker that plays kick motion then suspends itself.

---
+++ clfsm/machines/RoboCup2013/SMKicker.machine/SMKicker_VarRefs.mm
@@ -0,0 +1,16 @@
+//
+// SMKicker_VarRefs.mm
+//
+// Automatically created through MiCASE -- do not change manually!
+//
+SMKicker *_m = static_cast<SMKicker *>(_machine);
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-variable"
+#pragma clang diagnostic ignored "-Wshadow"
+
+WALK_Command_t	&wb_WalkInterface = _m->wb_WalkInterface;	///< 
+MOTION_SDK_Commands_t	&wb_MotionCommand = _m->wb_MotionCommand;	///< 
+MOTION_SDK_Status_t	&wb_MotionStatus = _m->wb_MotionStatus;	///< 
+
+#pragma clang diagnostic pop
---
+++ clfsm/machines/RoboCup2013/SMKicker.machine/State_Disable_DCM_Variables.h
@@ -0,0 +1,5 @@
+//
+// State_Disable_DCM_Variables.h
+//
+// Automatically created through MiCASE -- do not change manually!
+//
