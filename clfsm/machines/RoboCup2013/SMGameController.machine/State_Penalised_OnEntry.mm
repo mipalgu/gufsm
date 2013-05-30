7fa6ab244fc7a111c2d5cc90815cbd36bd0c2873
Rene Hexel <r.hexel@griffith.edu.au>
2013-05-30 11:51:06 +1000

    revert back to 10:46 version

--- clfsm/machines/RoboCup2013/SMGameController.machine/State_Penalised_OnEntry.mm
+++ clfsm/machines/RoboCup2013/SMGameController.machine/State_Penalised_OnEntry.mm
@@ -11,5 +11,5 @@ say("Paynalised");
 penalisedThroughChestButton = nao_state.chest_pressed() > 0;
 
 nao_state = nao_state_ptr.get();
-smGameState.setMyPenalty(myNumber,ManualButtonPenalty);
+//smGameState.set_amIPenalized(myNumber);
 postGS(smGameState);
