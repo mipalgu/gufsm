931ab5c79348c0a40beb2c11de7651ccaca5b1a1
Carl Lusty <you@example.com>
2013-06-14 17:03:22 +1000

    Now setting head stiffness when the head scanner restarts

--- clfsm/machines/RoboCup2013/SMHeadScanner.machine/SMHeadScanner_Includes.h
+++ clfsm/machines/RoboCup2013/SMHeadScanner.machine/SMHeadScanner_Includes.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include "CLMacros.h"
 #include "typeClassDefs/HAL_HeadTarget.h"
+#include "typeClassDefs/MOTION_SDK_Interface.h"
 #include "CLWhiteboard.h"
 
 
--- clfsm/machines/RoboCup2013/SMHeadScanner.machine/State_Start_OnEntry.mm
+++ clfsm/machines/RoboCup2013/SMHeadScanner.machine/State_Start_OnEntry.mm
@@ -1,3 +1,9 @@
 #ifdef DEBUG
 fprintf(stderr, "State: %s\n", state_name());
 #endif
+
+MOTION_SDK_Commands_t motion_ptr;
+MOTION_SDK_Interface motion;
+motion.set_head_stiffness(true);
+motion.set_head_stiffness_mask(true);
+motion_ptr(motion);
