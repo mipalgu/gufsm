1e96094d888161847dff7f73a4f545228a8f3578
Rene Hexel <r.hexel@griffith.edu.au>
2013-06-18 14:59:14 +1000

    make SMGameController compile again

--- clfsm/machines/RoboCup2013/SMGameController.machine/State_Unpenalise_OnEntry.mm
+++ clfsm/machines/RoboCup2013/SMGameController.machine/State_Unpenalise_OnEntry.mm
@@ -1,4 +1,9 @@
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wc++98-compat-pedantic"
+
 walk_post(WALK_Ready); protected_msleep(15); walk_post(WALK_ControlStatus(WALK_Run, 100, 0, 0, 100)); // engage DCM, walk a metre XXX: stalls all machines!
+
+#pragma clang diagnostic pop
 
 controlstatus = walk_status();
 odo_forward = controlstatus.odometry().forward;
