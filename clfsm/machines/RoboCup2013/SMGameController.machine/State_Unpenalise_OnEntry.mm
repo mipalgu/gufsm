a0f8d2344113c5960e377880b6f6436e96c29042
Rene Hexel <r.hexel@griffith.edu.au>
2013-06-18 14:56:11 +1000

    engage DCM and walk a metre in unpenalise

--- clfsm/machines/RoboCup2013/SMGameController.machine/State_Unpenalise_OnEntry.mm
+++ clfsm/machines/RoboCup2013/SMGameController.machine/State_Unpenalise_OnEntry.mm
@@ -1,2 +1,4 @@
+walk_post(WALK_Ready); protected_msleep(15); walk_post(WALK_ControlStatus(WALK_Run, 100, 0, 0, 100)); // engage DCM, walk a metre XXX: stalls all machines!
+
 controlstatus = walk_status();
 odo_forward = controlstatus.odometry().forward;
