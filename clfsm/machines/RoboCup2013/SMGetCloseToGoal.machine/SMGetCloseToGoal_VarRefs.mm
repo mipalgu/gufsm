0b8843115ff8ca7ed94ce7923f71f865b2a3b7f6
vlad <v.estivill-castro@griffith.edu.au>
2013-06-25 17:36:05 +0200

    Machines thatd eal with Goals go from examples to 2013

---
+++ clfsm/machines/RoboCup2013/SMGetCloseToGoal.machine/SMGetCloseToGoal_VarRefs.mm
@@ -0,0 +1,18 @@
+//
+// SMGetCloseToGoal_VarRefs.mm
+//
+// Automatically created through MiCASE -- do not change manually!
+//
+SMGetCloseToGoal *_m = static_cast<SMGetCloseToGoal *>(_machine);
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-variable"
+#pragma clang diagnostic ignored "-Wshadow"
+
+FilteredGoalSighting_t	&sightings_ptr = _m->sightings_ptr;	///< 
+FilteredVisionObject	&rightPost = _m->rightPost;	///< 
+FilteredVisionObject	&leftPost = _m->leftPost;	///< 
+FilteredVisionObject	&genericPost = _m->genericPost;	///< 
+FilteredVisionObject	&crossBar = _m->crossBar;	///< 
+
+#pragma clang diagnostic pop
