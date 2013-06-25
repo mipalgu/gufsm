f9cd955effca151dc45628802dae51b61a93db07
vlad <v.estivill-castro@griffith.edu.au>
2013-06-25 15:31:32 +0200

    Safer suspen in SMREADY, and removed dubbign code from aprticle filter

--- clfsm/machines/RoboCup2013/SMReady.machine/State_StartSpinPlayers_OnEntry.mm
+++ clfsm/machines/RoboCup2013/SMReady.machine/State_StartSpinPlayers_OnEntry.mm
@@ -1,4 +1,5 @@
 status = walk_status();
 currentOdometry = status.odometry().turn;
 walkStartOdometry = currentOdometry;
-walk_post(WALK_ControlStatus(WALK_Run,0,0,(float)(M_PI/4.0),100));
+float sign = (playerNumber %2) ? -1.0f : 1.0f ; // odd robots not the go lie spin the other way
+walk_post(WALK_ControlStatus(WALK_Run,0,0,(float)(sign*M_PI/4.0),100));
