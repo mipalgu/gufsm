6d18b6fd21c46fb0e93245658835d5da68cdafe0
Robert Coleman <robertc.softmyn@gmail.com>
2013-06-26 11:11:17 +0200

    SMGoalieSaver ready for more testing.

--- clfsm/machines/RoboCup2013/SMGoalieSaver.machine/SMGoalieSaver.h
+++ clfsm/machines/RoboCup2013/SMGoalieSaver.machine/SMGoalieSaver.h
@@ -16,12 +16,12 @@ namespace FSM
     {
         class SMGoalieSaver: public CLMachine
         {
-            CLState *_states[13];
+            CLState *_states[14];
         public:
             SMGoalieSaver(int mid  = 0, const char *name = "SMGoalieSaver");
             virtual ~SMGoalieSaver();
             virtual CLState * const * states() const { return _states; }
-            virtual int numberOfStates() const { return 13; }
+            virtual int numberOfStates() const { return 14; }
 #           include "SMGoalieSaver_Variables.h"
         };
     }
