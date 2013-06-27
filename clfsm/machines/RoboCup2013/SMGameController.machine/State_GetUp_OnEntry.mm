970a808653b0d9f40ad0516e577902c49ee8615c
Rene Hexel <r.hexel@griffith.edu.au>
2013-06-27 22:35:05 +0200

    Use get up from kneel in prepraration of motion module change

--- clfsm/machines/RoboCup2013/SMGameController.machine/State_GetUp_OnEntry.mm
+++ clfsm/machines/RoboCup2013/SMGameController.machine/State_GetUp_OnEntry.mm
@@ -1,3 +1,4 @@
+/*
 MOTION_SDK_Interface motion;
 motion.set_body_stiffness(true);
 motion.set_body_stiffness_mask(true);
@@ -5,3 +6,4 @@ motion.set_motion_player(Motions::get_up_from_kneel);
 motion.set_motion_player_mask(true);
 
 motion_ptr.set(motion);
+*/
