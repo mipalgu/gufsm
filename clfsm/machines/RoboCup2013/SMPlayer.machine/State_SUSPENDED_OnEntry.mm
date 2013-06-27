ae54e20be275d5a487e8c608c426c1144e009db5
Rene Hexel <r.hexel@griffith.edu.au>
2013-06-27 12:34:18 +0200

    revert back to player 5fa4d8ae4b74ebd11bc478893c138457724a6a97

--- clfsm/machines/RoboCup2013/SMPlayer.machine/State_SUSPENDED_OnEntry.mm
+++ clfsm/machines/RoboCup2013/SMPlayer.machine/State_SUSPENDED_OnEntry.mm
@@ -3,9 +3,6 @@ suspend("SMBallFollower");
 suspend("SMHeadBallTracker");
 suspend("SMKicker");
 
-if (playerMachine != CLError && control_machine_at_index(playerMachine, CLSuspend) == CLError)
-	cerr << "Error suspending " << playerMachine << " at index " << playerMachine << endl;
-
 if(walk_status.get().controlStatus() != WALK_Disconnected)
 {
 	walk_post(WALK_Stop);
--- clfsm/machines/RoboCup2013/SMPlayer.machine/State_StartPlayerX_OnEntry.mm
+++
@@ -1,2 +0,0 @@
-if (control_machine_at_index(playerMachine, CLRestart) == CLError)
-	cerr << "Error restarting " << playerMachine << " at index " << playerMachine << endl;
