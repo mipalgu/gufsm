49a735c242eee6d213a2f1e40a9b816f2939042d
vlad <v.estivill-castro@griffith.edu.au>
2013-06-27 22:40:16 +0200

    Monotonous speed decrease and more smoothing 7/8 to 1/8

--- clfsm/machines/RoboCup2013/SMBallFollower.machine/State_Short_Straight_OnEntry.mm
+++ clfsm/machines/RoboCup2013/SMBallFollower.machine/State_Short_Straight_OnEntry.mm
@@ -1,4 +1,4 @@
 float wantedSpeed = float(fminf(20.0f+7.0f*float(ball.distance())/float(limiter), 50.0f));
-forwardSpeed = 0.75f*forwardSpeed + 0.25f*wantedSpeed;
+forwardSpeed = (0.875f*forwardSpeed + 0.125f*wantedSpeed)> forwardSpeed ? forwardSpeed : 0.875f*forwardSpeed + 0.125f*wantedSpeed ;
 
 walk_post(WALK_ControlStatus(WALK_Run, forwardSpeed, 0, 0, 50));
