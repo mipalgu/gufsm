964f0bfd7f421ff0bc1fb3985a6e09e87e63593c
Rene Hexel <r.hexel@griffith.edu.au>
2013-06-27 09:32:08 +0200

    Fix angle signedness bug in SMReadyFromInitial

--- clfsm/machines/RoboCup2013/SMReadyFromInitial.machine/State_Turn_Head_To_Goal_OnEntry.mm
+++ clfsm/machines/RoboCup2013/SMReadyFromInitial.machine/State_Turn_Head_To_Goal_OnEntry.mm
@@ -1,6 +1,7 @@
 const TopParticles top_particles = particles.get();
 const ParticlePosition &top_particle = top_particles.particles(0);
 targetHeading = top_particle.heading();
+if (targetHeading > M_PI) targetHeading -= float(2*M_PI);
 
 head.GoToWithTime(int(RAD2DEG(headAngleX)), 0, headDelayInUS);
 head_ptr.set(head);
