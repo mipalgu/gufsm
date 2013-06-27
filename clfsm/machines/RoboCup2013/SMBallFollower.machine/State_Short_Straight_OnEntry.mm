6398409dbaf522dc406e010ca4137e768859e301
Rene Hexel <r.hexel@griffith.edu.au>
2013-06-27 15:36:25 +0200

    A bit more smoothing for the ball follower

--- clfsm/machines/RoboCup2013/SMBallFollower.machine/State_Short_Straight_OnEntry.mm
+++ clfsm/machines/RoboCup2013/SMBallFollower.machine/State_Short_Straight_OnEntry.mm
@@ -1,4 +1,4 @@
 float wantedSpeed = float(fminf(20.0f+7.0f*float(ball.distance())/float(limiter), 50.0f));
-forwardSpeed = 0.5f*forwardSpeed + 0.5f*wantedSpeed;
+forwardSpeed = 0.75f*forwardSpeed + 0.25f*wantedSpeed;
 
 walk_post(WALK_ControlStatus(WALK_Run, forwardSpeed, 0, 0, 50));
