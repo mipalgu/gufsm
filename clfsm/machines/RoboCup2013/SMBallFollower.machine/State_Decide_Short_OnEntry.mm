2ec55a223c7aad97495831b98dadc0c6fe526f28
vlad <v.estivill-castro@griffith.edu.au>
2013-06-28 15:18:35 +0200

    Swap to specialized calibration when close

--- clfsm/machines/RoboCup2013/SMBallFollower.machine/State_Decide_Short_OnEntry.mm
+++ clfsm/machines/RoboCup2013/SMBallFollower.machine/State_Decide_Short_OnEntry.mm
@@ -3,3 +3,12 @@ fprintf(stderr, "State: %s\n", state_name());
 #endif
 
 shortDistanceToBall = true;
+
+VisionControlStatus aCheck;
+aCheck=vision_status.get();
+if (aCheck.loadCalibration()== Calibration0)
+{ VisionControlStatus message;
+message.set_loadCalibration(Calibration1);
+vision_ptr.set(message);
+
+	}
