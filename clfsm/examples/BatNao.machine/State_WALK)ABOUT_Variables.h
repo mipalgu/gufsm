d48154d00a137c0defa8b2957d7f942f1bb8822f
vlad <v.estivill-castro@griffith.edu.au>
2013-11-28 19:35:21 +0100

    Bat walks forward until chest pressed

--- clfsm/examples/BatNao.machine/State_WALK)ABOUT_Variables.h
+++
@@ -1,5 +0,0 @@
-//
-// State_WALK)ABOUT_Variables.h
-//
-// Automatically created through MiCASE -- do not change manually!
-//
--- clfsm/examples/BatNao.machine/State_WALK_ABOUT_OnEntry.mm
+++ clfsm/examples/BatNao.machine/State_WALK_ABOUT_OnEntry.mm
@@ -3,3 +3,6 @@ fprintf(stderr, "STATE: %s\n", state_name());
 #endif
 say("Bat Man");
 nao_state = nao_state_ptr.get();
+
+walk_post(WALK_Ready); protected_msleep(15);
+walk_post(WALK_ControlStatus(WALK_Run, 35, 0, 0, 50));
