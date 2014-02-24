73d10b9550961a32c395bdadd260f3f91a4fafdb
Eugene Gilmore <eugene.gilmore@griffithuni.edu.au>
2014-02-24 11:21:22 +1000

    fix head stiffness bug while walking in teleoperation machine

--- clfsm/machines/RoboCup2014/SMTeleoperationHeadControl.machine/State_HeadStop_OnEntry.mm
+++ clfsm/machines/RoboCup2014/SMTeleoperationHeadControl.machine/State_HeadStop_OnEntry.mm
@@ -1,4 +1,5 @@
 MOTION_Commands motion;
 motion.set_head_stiffness(0);
+motion.set_head_stiffness_mask(1);
 motionPtr->post(motion);
 remoteControl=remoteControlPtr->get();
--- clfsm/machines/RoboCup2014/SMTeleoperationHeadControl.machine/State_Walking_OnEntry.mm
+++ clfsm/machines/RoboCup2014/SMTeleoperationHeadControl.machine/State_Walking_OnEntry.mm
@@ -1,1 +1,5 @@
 remoteControl=remoteControlPtr->get();
+MOTION_Commands motion;
+motion.set_head_stiffness(1);
+motion.set_head_stiffness_mask(1);
+motionPtr->post(motion);
