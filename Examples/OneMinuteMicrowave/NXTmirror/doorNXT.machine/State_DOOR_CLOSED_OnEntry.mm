7a06f1421a45c0843544b64071f4834b52e0f41e
vlad <v.estivill-castro@griffith.edu.au>
2014-02-24 22:37:28 +1000

    this machine does the door in the right order

--- Examples/OneMinuteMicrowave/NXTmirror/doorNXT.machine/State_DOOR_CLOSED_OnEntry.mm
+++ Examples/OneMinuteMicrowave/NXTmirror/doorNXT.machine/State_DOOR_CLOSED_OnEntry.mm
@@ -6,6 +6,6 @@ fprintf(stderr,"STATE: %s\n",state_name());
  wb.addMessage("doorOpen", WBMsg(false), true);
 WEBOTS_NXT_bridge theInfo= touch_data_ptr.get();
 if ( (TOUCH==theInfo.theInstruction()  )&& (theInfo.firstParameter()==RIGHT_TOUCH_SENSOR))
-     doorOpen= theInfo.secondParameter();
+     doorOpen= !theInfo.secondParameter();
 else
-     doorOpen=false;
+     doorOpen=true;
