cfef62e5b2da7da9ebfe30ecdef08501200bd15b
Vlad Estivill-Castro <v.estivill-castro@griffith.edu.au>
2014-03-21 16:55:19 +1000

    Bell now works, it uses the new messages of control/status

--- Examples/OneMinuteMicrowave/NXTmirror/BellNXT.machine/BellNXT_VarRefs.mm
+++ Examples/OneMinuteMicrowave/NXTmirror/BellNXT.machine/BellNXT_VarRefs.mm
@@ -1,7 +1,7 @@
 //
-// BellNXT_VarRefs.mm
+//BellNXT_VarRefs.mm
 //
-// Automatically created through MiCASE -- do not change manually!
+//Automatically created through MiEditCLFSM -- do not change manually!
 //
 BellNXT *_m = static_cast<BellNXT *>(_machine);
 
@@ -9,17 +9,17 @@ BellNXT *_m = static_cast<BellNXT *>(_machine);
 #pragma clang diagnostic ignored "-Wunused-variable"
 #pragma clang diagnostic ignored "-Wshadow"
 
-int16_t	&C = _m->C;	///< noteC
-int16_t	&D = _m->D;	///< 
-int16_t	&E = _m->E;	///< 
-int16_t	&G = _m->G;	///< 
-int16_t	&R = _m->R;	///< 
-int	&songLength = _m->songLength;	///< 
-int	&currentNote = _m->currentNote;	///< 
-std::vector<int16_t>	&song = _m->song;	///< 
-Print_t	&print_ptr = _m->print_ptr;	///< 
-WEBOTS_NXT_bridge_t	&a_Command_Handler = _m->a_Command_Handler;	///< 
-int	&bellSignal = _m->bellSignal;	///< 
-guWhiteboard::Whiteboard	&wb = _m->wb;	///< 
+int16_t	&C= _m->C;	///<noteC
+int16_t	&D= _m->D;	///<
+int16_t	&E= _m->E;	///<
+int16_t	&G= _m->G;	///<
+int16_t	&R= _m->R;	///<
+int	&songLength= _m->songLength;	///<
+int	&currentNote= _m->currentNote;	///<
+std::vector<int16_t>	&song= _m->song;	///<
+Print_t	&print_ptr= _m->print_ptr;	///<
+WEBOTS_NXT_vector_bridge_t	&a_Command_Handler= _m->a_Command_Handler;	///<
+int	&bellSignal= _m->bellSignal;	///<
+guWhiteboard::Whiteboard	&wb= _m->wb;	///<
 
 #pragma clang diagnostic pop
