feb44142cb3bd7f25efff65c627216ffaf870bb1
Rene Hexel <r.hexel@griffith.edu.au>
2014-03-21 20:31:54 +1000

    Squash another VLA language warning

--- clfsm/clfsm_machine.cc
+++ clfsm/clfsm_machine.cc
@@ -80,6 +80,7 @@
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wc++98-compat-pedantic"
 #pragma clang diagnostic ignored "-Wvla"
+#pragma clang diagnostic ignored "-Wvla-extension"
 #pragma clang diagnostic ignored "-Wexit-time-destructors"
 #pragma clang diagnostic ignored "-Wunused-function"
 
