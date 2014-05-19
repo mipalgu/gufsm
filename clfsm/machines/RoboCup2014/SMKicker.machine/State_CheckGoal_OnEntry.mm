506a4f799c6a1d50425509b2a39aa602a07c128c
Eugene Gilmore <eugene.gilmore@griffithuni.edu.au>
2014-05-19 13:57:56 +1000

    fix bug after update to visionball wb message, fix compilation error in kicker state machine

--- clfsm/machines/RoboCup2014/SMKicker.machine/State_CheckGoal_OnEntry.mm
+++ clfsm/machines/RoboCup2014/SMKicker.machine/State_CheckGoal_OnEntry.mm
@@ -1,7 +1,7 @@
 FilteredGoalSighting_t goals_ptr;
 FilteredOneDimObjects goals = goals_ptr.get();
 
-for (FilteredVisionObjectType i = FVOGoalCrossBar; i >= FVOGoalPost; i--)
+for (FilteredVisionObjectType i = FVOGoalCrossBar; i >= FVOGoalPost; i=(FilteredVisionObjectType)((int)i-1))
 {
     goal = goals.get_object(i);	
 	goal_index = i;
