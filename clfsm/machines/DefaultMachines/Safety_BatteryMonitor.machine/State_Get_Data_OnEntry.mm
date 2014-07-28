d13b697d75734afc24969840ce33867a302f1161
Carl Lusty <carl.lusty@griffithuni.edu.au>
2014-07-28 17:17:45 +1000

    Warnings

--- clfsm/machines/DefaultMachines/Safety_BatteryMonitor.machine/State_Get_Data_OnEntry.mm
+++ clfsm/machines/DefaultMachines/Safety_BatteryMonitor.machine/State_Get_Data_OnEntry.mm
@@ -1,5 +1,5 @@
 {
 		body_sensors = body_sensors_t.get();
-		leds = leds_t.get();								leds.LEDsGroupChange(Ears, (LEDColour)(body_sensors.Battery_Charge()*10));
+		leds = leds_t.get();								leds.LEDsGroupChange(Ears, static_cast<LEDColour>((body_sensors.Battery_Charge()*10)));
 		leds_t.set(leds);	
 }
