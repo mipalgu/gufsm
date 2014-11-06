09215d124b34a550c1b977abf808712bde77a39c
Jeremy Cade <jeremy.cade@griffithuni.edu.au>
2014-11-06 15:06:37 +1000

    Fix old-style casts

--- clfsm/examples/EpuckFollowsLine.machine/State_SET_MOTORS_SPEED_OnEntry.mm
+++ clfsm/examples/EpuckFollowsLine.machine/State_SET_MOTORS_SPEED_OnEntry.mm
@@ -3,6 +3,6 @@ std::string stateName("STATE: ");
 stateName+=state_name();
 print_ptr(stateName);
 #endif
-WEBOTS_NXT_bridge thetMotorCommand(robotID,MOVE_MOTORS,(int16_t) (leftSpeed/maxSpeed), (int16_t) (rightSpeed/maxSpeed),false);
+WEBOTS_NXT_bridge thetMotorCommand(robotID,MOVE_MOTORS,static_cast<int16_t>(leftSpeed/maxSpeed), static_cast<int16_t>(rightSpeed/maxSpeed),false);
 //post the speed
 a_Command_Handler.set(thetMotorCommand);
