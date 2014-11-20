d143bd136f3dc03a724b0e979b52cf4663603bc3
Rene Hexel <r.hexel@griffith.edu.au>
2014-11-20 12:26:12 +1000

    Add directory for regression test machines

---
+++ clfsm/test/SpeedTest.machine/SpeedTest_VarRefs.mm
@@ -0,0 +1,14 @@
+//
+// SpeedTest_VarRefs.mm
+//
+// Automatically created through MiCASE -- do not change manually!
+//
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-variable"
+#pragma clang diagnostic ignored "-Wshadow"
+
+SpeedTest *_m = static_cast<SpeedTest *>(_machine);
+
+int32_t	&count = _m->count;	///< Simple Counter
+
+#pragma clang diagnostic pop
---
+++ clfsm/test/SpeedTest.machine/State_Initial_OnEntry.mm
@@ -0,0 +1,5 @@
+count = 0;
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, static_cast<int64_t>(5 * NSEC_PER_SEC)), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0UL), ^{
+	    std::cout << "failed." << std::endl;
+            exit(EXIT_FAILURE);
+        });
