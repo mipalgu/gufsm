abba00f999f06ca4276be98ebcdfa2b2005edaae
Rene Hexel <r.hexel@griffith.edu.au>
2015-07-27 14:00:32 +1000

    Update PingPongCLFSM machine to modern CLmacros

--- clfsm/examples/PingPongCLFSM.machine/State_Pong_OnEntry.mm
+++ clfsm/examples/PingPongCLFSM.machine/State_Pong_OnEntry.mm
@@ -1,9 +1,3 @@
-{
-  using namespace std;
-        
-        static int count = 0;
-        
-  stateName = "Pong";
-  currentState = 1;
-    cout << stateName << " " << static_cast<int>(fmod(static_cast<double>(current_time_in_microseconds() / 1000000.0L), 100)) << endl;
-}
+cout << state_name() << "\t"
+     << machine_id() << "/" number_of_machines()
+     << ": " << static_cast<int>(fmod(static_cast<double>(current_time_in_microseconds() / 1000000.0L), 100)) << endl;
