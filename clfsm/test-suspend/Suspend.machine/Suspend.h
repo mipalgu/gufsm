a537ab01c84b08ea950200612ba9b638f9901fb2
Rene Hexel <r.hexel@griffith.edu.au>
2015-08-01 16:47:34 +1000

    Add suspend test

--- clfsm/test-suspend/Suspend.machine/Suspend.h
+++ clfsm/test-suspend/Suspend.machine/Suspend.h
@@ -3,8 +3,8 @@
 //
 // Automatically created through MiCASE -- do not change manually!
 //
-#ifndef __clfsm__Suspend__
-#define __clfsm__Suspend__
+#ifndef clfsm_Suspend_
+#define clfsm_Suspend_
 
 #include "CLMachine.h"
 
@@ -32,4 +32,4 @@ extern "C"
     FSM::CLM::Suspend *CLM_Create_Suspend(int mid, const char *name);
 }
 
-#endif // defined(__gufsm__Suspend__)
+#endif // defined(clfsm_Suspend_)
