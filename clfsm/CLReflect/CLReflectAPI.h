7d36cc445b1fbc598db117c007abdf33c81c7ce0
Mitch Wenman <mitchwenman@gmail.com>
2015-08-08 13:00:39 +1000

    started testing meta-machine creation

--- clfsm/CLReflect/CLReflectAPI.h
+++ clfsm/CLReflect/CLReflectAPI.h
@@ -1,5 +1,6 @@
 extern "C" {
     #include "API_Result.h"
+    #include "API_Util.h"
     #include "CLReflectFunctionPointerTypes.h"
     #include "API_Interface.h"
     #include "API_MetaMachine.h"
