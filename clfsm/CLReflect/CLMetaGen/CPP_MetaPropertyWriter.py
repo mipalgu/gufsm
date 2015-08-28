f150c273f48da6d2c19149d184b15a30524d7528
Mitch Wenman <mitchwenman@gmail.com>
2015-08-28 14:24:35 +1000

    reset deleted file

---
+++ clfsm/CLReflect/CLMetaGen/CPP_MetaPropertyWriter.py
@@ -0,0 +1,15 @@
+from CodeGen import *
+
+class CPP_MetaPropertyWriter(object):
+    """docstring for CPP_MetaPropertyWriter"""
+    def __init__(self, cpp):
+        super(CPP_MetaPropertyWriter, self).__init__()
+        self.cpp = cpp
+
+    def writeDeclarations(self):
+        pass
+
+    def writeImplementations(self):
+        pass
+
+            
