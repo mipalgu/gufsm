81102beabbfea1de4e312470626c163165b33fac
Mitch Wenman <mitchwenman@gmail.com>
2015-08-28 10:54:58 +1000

    added parser to dependency manager

---
+++ clfsm/CLReflect/CLMetaGen/CLMetaGenWriter.py
@@ -0,0 +1,5 @@
+class CLMetaGenWriter(object):
+    """docstring for CLMetaGenWriter"""
+    def __init__(self, machineDef):
+        super(CLMetaGenWriter, self).__init__()
+        self.machineDef = machineDef
--- clfsm/CLReflect/CLMetaGen/CPP_Parser.py
+++ clfsm/CLReflect/CLMetaGen/CPP_Parser.py
@@ -1,11 +1,11 @@
 # Parses the directory and creates a machine definition
 from CPP_MetaMachineDefinition import *
+from CLMetaGenParser import *
 
-class CPP_Parser:
-
+class CPP_Parser(CLMetaGenParser):
+    """docstring for CPP_Parser"""
     def __init__(self, path, machineName):
-        self._path = path
-        self._machineName = machineName
+        super(CPP_Parser, self).__init__(path, machineName)
 
     def parse(self):
         self._metaMachine = CPP_MetaMachineDefinition(self._path, self._machineName)
