e18953e66fcd225a02acc282c3017d49725149ad
Mitch Wenman <mitchwenman@gmail.com>
2015-08-28 15:53:12 +1000

    void properties

--- clfsm/examples/CLReflectDemo.machine/CLReflectDemo_VarRefs.mm
+++ clfsm/examples/CLReflectDemo.machine/CLReflectDemo_VarRefs.mm
@@ -10,7 +10,7 @@ CLReflectDemo *_m = static_cast<CLReflectDemo *>(_machine);
 #pragma clang diagnostic ignored "-Wshadow"
 
 refl_metaMachine	&metaMachine= _m->metaMachine;	///<
-unsigned int	&currentMachineID= _m->currentMachineID;	///<
-unsigned int	&numberOfMachines= _m->numberOfMachines;	///<
+unsignedint	&currentMachineID= _m->currentMachineID;	///<
+unsignedint	&numberOfMachines= _m->numberOfMachines;	///<
 
 #pragma clang diagnostic pop
--- clfsm/examples/CLReflectDemo.machine/CLReflectDemo_Variables.h
+++ clfsm/examples/CLReflectDemo.machine/CLReflectDemo_Variables.h
@@ -4,5 +4,5 @@
 //Automatically created through MiEditCLFSM -- do not change manually!
 //
 refl_metaMachine	metaMachine;	///<
-unsigned int	currentMachineID;	///<
-unsigned int	numberOfMachines;	///<
+unsignedint	currentMachineID;	///<
+unsignedint	numberOfMachines;	///<
