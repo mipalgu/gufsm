c2f0b45614e2aa71ba2df84af6fffef6ea23533b
Mitch Wenman <mitchwenman@gmail.com>
2015-09-09 09:14:01 +1000

    state variables now compiling

--- clfsm/examples/CLReflectDemo.machine/CLReflectDemo_Variables.h
+++ clfsm/examples/CLReflectDemo.machine/CLReflectDemo_Variables.h
@@ -3,3 +3,7 @@
 //
 //Automatically created through MiEditCLFSM -- do not change manually!
 //
+refl_metaMachine	metaMachine;	///<
+unsigned int	currentMachineID;	///<
+unsigned int*	numberCopy;	///<
+unsigned int	numberOfMachines;	///<
--- clfsm/examples/CLReflectDemo.machine/State_Variables_OnEntry.mm
+++ clfsm/examples/CLReflectDemo.machine/State_Variables_OnEntry.mm
@@ -4,7 +4,7 @@ std::cout << "I have " << numProperties << " variables" << std::endl;
 refl_metaProperty * properties = refl_getMachineMetaProperties(metaMachine, NULL);
 for (unsigned int i = 0; i < numProperties; i++)
 {
-	std::string type(refl_getMetaPropertyType(properties[i], NULL));
+	std::string type(refl_getMetaPropertyTypeAsString(properties[i], NULL));
 	std::string name(refl_getMetaPropertyName(properties[i], NULL));
 	void * address = refl_getMachinePropertyValue_V(metaMachine, i, NULL);
 	char stringValue[40];
