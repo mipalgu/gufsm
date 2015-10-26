a4dad7567278be3f865c3a0a36f8aec3d38f6060
Mitch Wenman <mitchwenman@gmail.com>
2015-10-26 17:53:19 +1000

    comments

--- clfsm/CLReflect/API_Util.h
+++ clfsm/CLReflect/API_Util.h
@@ -9,6 +9,14 @@
 
 #include "API_Result.h"
 
+/*!
+    Copies a string, including the null terminating character.
+    @param dest The destination string.
+    @param src The source string
+    @param size The length of the /p dest buffer.
+    @return REFL_SUCCESS if all goes well.
+            REFL_BUFFER_OVERFLOW if dest is too small.
+*/
 CLReflectResult refl_strcpy(char* dest, char const * src, size_t size);
 
 #endif /* end of include guard: API_UTIL_H */
