f89cc34e5eed2edc8547bec5e5db272c4e4372c4
Callum McColl <callum.mccoll1@gmail.com>
2016-11-14 05:49:22 +1000

    Made refl_TRUE and refl_FALSE the same type as refl_bool

--- clfsm/CLReflect/CLReflectFunctionPointerTypes.h
+++ clfsm/CLReflect/CLReflectFunctionPointerTypes.h
@@ -20,9 +20,13 @@ typedef void* refl_machine_t;
 typedef void* refl_userData_t;
 typedef unsigned char refl_bool;
 
-#define refl_TRUE 1
-#define refl_FALSE 0
-
+#ifdef __cplusplus
+#define refl_TRUE (static_cast<refl_bool>(1))
+#define refl_FALSE (static_cast<refl_bool>(0))
+#else
+#define refl_TRUE ((refl_bool) 1)
+#define refl_FALSE ((refl_bool) 0)
+#endif
 // Function pointers
 
 //! Gets a machine as a void pointer given its name
