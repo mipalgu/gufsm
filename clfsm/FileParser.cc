8d311ad0dfd56613481da1397e1d12b45e372603
Morgan McColl <morgan.mccoll92@gmail.com>
2018-05-11 11:45:02 +1000

    Removed prints.

--- clfsm/FileParser.cc
+++ clfsm/FileParser.cc
@@ -12,24 +12,17 @@ using namespace FSM;
 
 FileParser::FileParser(string path) {
     this->_contents = string_from_file(path.c_str());
-    cout << "Got Contents." << endl;
     vector<string> lines = components_of_string_separated(this->_contents, '\n', false);
-    cout << "Got Lines" << endl;
     for (unsigned long i = 0; i < lines.size(); i++) {
-        cout << "Parsing line " << i << endl;
         this->parseLine(lines[i]);
     }
 }
 
 void FileParser::parseLine(string line) {
     this->_raws.push_back(line);
-    cout << "Pushed back raw" << endl;
     this->_paths.push_back(this->parsePath(line));
-    cout << "Pushed back paths." << endl;
     this->_names.push_back(this->parseName(line));
-    cout << "Pushed back name" << endl;
     this->_durations.push_back(this->parseDuration(line));
-    cout << "Pushed back duration." << endl;
 }
 
 
