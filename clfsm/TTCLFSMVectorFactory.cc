f26eec306cf2254f0e768edf1884c8cdcc88163b
Morgan McColl <morgan.mccoll92@gmail.com>
2018-06-21 17:10:49 +1000

    Trying to fix errors.

--- clfsm/TTCLFSMVectorFactory.cc
+++ clfsm/TTCLFSMVectorFactory.cc
@@ -62,7 +62,7 @@ void TTCLFSMVectorFactory::executeTT(
         if (
             !this->executeOnceTT(
                 should_execute_machine,
-                scheduler->createSchedule(names, periods, deadlines);
+                scheduler->createSchedule(names, periods, deadlines),
                 context,
                 accepting_action
             )
@@ -119,7 +119,7 @@ long long TTCLFSMVectorFactory::sleepTillTimeslot(long long scheduled) {
     if (scheduled <= now) {
         return now;
     }
-    usleep(scheduled - now);
+    usleep(static_cast<unsigned int>(scheduled - now));
     return this->getTimeUS();
 }
 
